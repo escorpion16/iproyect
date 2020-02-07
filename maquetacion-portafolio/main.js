d7fe9e61d1bbdf808f7ca2d8e1123af9b9990e68
dennis <denizponce16@gmail.com>
2020-02-06 21:19:16 -0500

    proyecto finalizado

---
+++ maquetacion-portafolio/main.js
@@ -0,0 +1,18 @@
+function one_click1(){
+    alert("Web Development");
+}
+function one_click2(){
+    alert("Digital Marketing");
+}
+function one_click3(){
+    alert("Branding & Identity");
+}
+function one_click4(){
+    alert("Customizability");
+}
+function one_click5(){
+    alert("Responsive");
+}
+function one_click6(){
+    alert("Photography");
+}
